052f2432bfaaa37626f2e7edac09222bc7d99933
manojkumar-nielseniq <manoj.x.kumar@nielseniq.com>
2022-07-31 10:43:02 +0530

    FIrst commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponentComponent } from './nav-component/nav-component.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { TemplComponent } from './templ/templ.component';
+import { TableComponent } from './table/table.component';
+import { FindMyStuffComponent } from './find-my-stuff/find-my-stuff.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponentComponent,
+    HeroSectionComponent,
+    TemplComponent,
+    TableComponent,
+    FindMyStuffComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
