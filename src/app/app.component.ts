1d1c21115402b3e9a73b1d64a145e97a57b7b484
akash-niq <akash.x.a@nielseniq.com>
2022-08-09 12:02:25 +0530

    responsive2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dtk-report';
-
-}
+export class AppComponent {}
