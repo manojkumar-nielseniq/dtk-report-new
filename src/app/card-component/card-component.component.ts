461a4c0c95231b6e478ad9fcf3dc3188eb27a178
manojkumar-nielseniq <manoj.x.kumar@nielseniq.com>
2022-08-10 09:35:15 +0530

    Revert "Updated CSS"
    This reverts commit 230ad090499f9dc9c4d4cc87b88d27c2de034807, reversing
    changes made to 6a6d3feea2321cf48d8d9c55a6c3d8674fe7051c.

--- src/app/card-component/card-component.component.ts
+++ src/app/card-component/card-component.component.ts
@@ -18,17 +18,11 @@ export class CardComponentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-<<<<<<< HEAD
     // this.incomeNum = this.incomeData.income;
     // console.log(this.incomeData);
     
     
   }
-=======
-    
-  }
-  MAX_NUM = Number.MAX_VALUE;
->>>>>>> 4d8dd56fd230044da934b20a80fc9ec8462bd5e3
 
   ngOnChanges(changes: SimpleChanges): void {
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
--- src/app/data-service/data-service.service.ts
+++ src/app/data-service/data-service.service.ts
@@ -7,12 +7,7 @@ import { Injectable } from '@angular/core';
 export class DataServiceService {
 
   constructor(private http: HttpClient) {} 
-<<<<<<< HEAD
   chartDataResponse(dataIncome: string, dataBuying: string){
     return this.http.get("http://localhost:8081/niq/getdata?buyingType="+dataBuying+"&incomeType="+dataIncome)//+"&manufacturers="+dataMfr)
-=======
-  chartDataResponse(dataIncome: string, dataBuying: string, dataMfr:string){
-    return this.http.get("http://localhost:8081/niq/getdata?buyingType="+dataBuying+"&incomeType="+dataIncome+"&manufacturers="+dataMfr)
->>>>>>> 4d8dd56fd230044da934b20a80fc9ec8462bd5e3
   };
 }
